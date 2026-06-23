25e81dc6f7d5fa34fa87a44496d8e02dd7b4c660
Neev <groverneev@gmail.com>
2026-06-23 11:40:44 -0700

    started this program

---
+++ C++/USACO_Bronze_Guide/AirCownditioningII.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int N, M;
+    cin >> N;
+    cin >> M;
+
+    vector<vector<int>> cows(N);
+    vector<vector<int>> ACs(M);
+
+    int a, b, c, d;
+    for (vector<int> v : cows) {
+        cin >> a >> b >> c;
+        v.push_back(a);
+        v.push_back(b);
+        v.push_back(c);
+    }
+    for (vector<int> v : ACs) {
+        cin >> a >> b >> c >> d;
+        v.push_back(a);
+        v.push_back(b);
+        v.push_back(c);
+        v.push_back(d);
+    }
+
+    
+
+    return 0;
+}
