eae13a21147d68ca8ea9d24165f0c9daa028916b
Neev Grover <127623629+groverneev@users.noreply.github.com>
2026-07-24 13:39:07 -0400

    finished graphs

--- C++/USACO_Bronze_Guide/Graphs/MilkFactory.cpp
+++ C++/USACO_Bronze_Guide/Graphs/MilkFactory.cpp
@@ -1,6 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+vector<set<int>> vec(105);
+
+void recurse(int x, set<int>& spots) {
+    for (const int& i : vec[x]) {
+        auto result = spots.insert(i);
+        if (result.second == true) { // not duplicate 
+            recurse(i, spots);
+        }
+    }
+}
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
@@ -10,33 +21,30 @@ int main() {
     int n;
     cin >> n;
 
-    vector<set<int>> vec(105);
-
     for (int i = 1; i < n; ++i) {
         int a, b;
         cin >> a >> b;
-        vec[b].insert(a);
+        vec[b].insert(a); // in b's list, we see that a is going into it
     }
 
     for (int i = 1; i <= n; ++i) {
-        set<int> addingSet;
         set<int> spotsVisited;
         spotsVisited.insert(i);
-        for (const int& num : vec[i]) {
-
+        recurse(i, spotsVisited);
+        if (spotsVisited.size() == n) {
+            cout << i;
+            return 0;
         }
     }
 
-    // resources
-    // https://usaco.org/index.php?page=viewproblem2&cpid=940
+    cout << -1;
+
     // https://csacademy.com/app/graph_editor/
     // cph graphs --- https://usaco.guide/CPH.pdf#page=119
     // https://apps.uworld.com/courseapp/collegeprep/v62/createtest/16670785
     // https://blog.prepscholar.com/sat-vocabulary-words
     // https://tableau.calstate.edu/views/Application_withsystemwide/AppAdmitEnroll?iframeSizedToWindow=true&%3Aembed=y&%3AshowAppBanner=false&%3Adisplay_count=no&%3Arender=true&%3AshowVizHome=no&%3Aorigin=viz_share_link
     // https://www.universityofcalifornia.edu/about-us/information-center/admissions-source-school
-
-    cout << -1;
 
     return 0;
 }
