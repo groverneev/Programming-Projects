daa8a4c28ee6ff892e53342faad1bc0ffc36b563
Neev Grover <127623629+groverneev@users.noreply.github.com>
2026-07-26 16:58:32 -0400

    did problem all by myself :)

---
+++ C++/USACO_Bronze_Guide/Photoshoot2.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+    vector<int> start(n);
+    vector<int> end(n);
+
+    for (int& i : start) {
+        cin >> i;
+    }
+
+    for (int& i : end) {
+        cin >> i;
+    }
+
+    int startIdx{};
+    int endIdx{};
+    set<int> mySet;
+    int counter{};
+
+    while (startIdx < n && endIdx < n) { // check this condition
+        if (start[startIdx] == end[endIdx]) {
+            ++startIdx;
+            ++endIdx;
+        }
+        else if (mySet.find(start[startIdx]) != mySet.end()) {
+            ++startIdx;
+        }
+        else if (start[startIdx] != end[endIdx]) {
+            ++counter;
+            mySet.insert(end[endIdx]);
+            ++endIdx;
+        }
+    }
+
+    cout << counter;
+
+    return 0;
+}
