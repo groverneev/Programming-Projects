b45a84254985acbbd61bd82ad4d3ab5f58ad066a
Neev <groverneev@gmail.com>
2026-08-05 17:23:45 -0700

    did program

---
+++ C++/SilverGuide/PrefixSums/StaticRangeSum.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+// Key learning: always use long long!!!
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    ll n, q;
+    cin >> n >> q;
+    vector<ll> vec(n + 4, 0);
+    vector<ll> prefix(n + 4, 0);
+    ll asdf;
+    for (ll i = 1; i <= n; ++i) {
+        cin >> asdf;
+        vec[i] = asdf;
+        prefix[i] = prefix[i - 1] + asdf;
+    }
+
+    ll l, r;
+
+    for (ll i = 0; i < q; ++i) {
+        cin >> l >> r;
+        cout << prefix[r] - prefix[l] << "\n";
+    }
+
+
+    return 0;
+}
