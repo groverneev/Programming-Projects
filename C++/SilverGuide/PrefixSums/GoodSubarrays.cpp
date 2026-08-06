1d49fafadc8a31b56f669010ad532b7313da9aa3
Neev <groverneev@gmail.com>
2026-08-05 22:40:20 -0700

    WIP

--- C++/SilverGuide/PrefixSums/GoodSubarrays.cpp
+++ C++/SilverGuide/PrefixSums/GoodSubarrays.cpp
@@ -11,11 +11,11 @@ int main() {
     for (int qw = 0; qw < t; ++qw) {
         int n;
         string s;
-        cin << n << s;
+        cin >> n >> s;
         vector<int> vec(n + 1);
         vector<int> prefix(n + 1);
         for (int i = 1; i <= n; ++i) {
-            int x = stoi(s[i]);
+            int x = stoi(s[i]); // string to int
             vec[i] = x;
             prefix[i] = prefix[i - 1] + x;
         }
